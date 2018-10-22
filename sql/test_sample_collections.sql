78aaaad3c8b499976dfdb00daf75982339fdb19c
Vipul Naik <vipulnaik1@gmail.com>
2018-10-22 08:15:15 -0700

    More file moves and structural changes for #1, also add stool test details

--- sql/test_sample_collections.sql
+++ sql/test_sample_collections.sql
@@ -5,12 +5,13 @@ create table test_sample_collections(
   `fasting` boolean,
   `collection_lab` varchar(255),
   `requester` varchar(255),
-  `notes` varchar(2000)
+  `notes` varchar(2000),
+  `report_filename` varchar(255)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-insert into test_sample_collections(collection_slug, collection_date, sample_type, fasting, collection_lab, requester, notes) values
-  ('December 2016 LabCorp gastro blood tests','2016-12-12','Blood',true,'LabCorp','Jeffrey M. Aron','A large number of blood tests were requested by the gastro doctor whom I visited because of stomach pains. Ultimately, no problems were discovered, and the stomach pains had improved after some diet changes, so no further action was necessary based on these tests'),
-  ('December 2016 LabCorp gastro stool tests','2016-12-17','Stool',true,'LabCorp','Jeffrey M. Aron','A few stool tests (along with a larger number of blood tests) were requested by the gastro doctor whom I visited because of stomach pains. Ultimately, no problems were discovered, and the stomach pains had improved after some diet changes, so no further action was necessary based on these tests'),
-  ('November 2017 Columbia Asia vitamin and lipid profile tests','2017-11-12','Blood',true,'Columbia Asia','Self','Tests for Vitamin B12, Vitamin D3, and basic lipid profile'),
-  ('November 2017 Metropolis Healthcare vitamin and TSH tests','2017-11-17','Blood',true,'Metropolis Healthcare','Self','Tests for Vitamin B12, Vitamin D3, and Thyroid Stimulating Hormone (TSH). The goal of the B12 and D3 tests was to validate and cross-check the low readings from the test done a few days before it at Columbia Asia'),
-  ('September 2018 LabCorp vitamin tests','2018-09-19','Blood',true,'LabCorp','Self (via Request A Test)','Tests for Vitamin B12 and Vitamin D3. The goal was to measure the effect of vitamin supplementation on vitamin levels after a few months of taking supplements. Prior to starting supplements, the last test (November 2017) had revealed low readings for both vitamins');
+insert into test_sample_collections(collection_slug, collection_date, sample_type, fasting, collection_lab, requester, notes, report_filename) values
+  ('December 2016 LabCorp gastro blood tests','2016-12-12','Blood',false,'LabCorp','Jeffrey M. Aron','A large number of blood tests were requested by the gastro doctor whom I visited because of stomach pains. Ultimately, no problems were discovered, and the stomach pains had improved after some diet changes, so no further action was necessary based on these tests','201612-labcorp-gastro-blood.pdf'),
+  ('December 2016 LabCorp gastro stool tests','2016-12-17','Stool',false,'LabCorp','Jeffrey M. Aron','A few stool tests (along with a larger number of blood tests) were requested by the gastro doctor whom I visited because of stomach pains. Ultimately, no problems were discovered, and the stomach pains had improved after some diet changes, so no further action was necessary based on these tests','201612-labcorp-gastro-stool.pdf'),
+  ('November 2017 Columbia Asia vitamin and lipid profile tests','2017-11-12','Blood',true,'Columbia Asia','Self','Tests for Vitamin B12, Vitamin D3, and basic lipid profile','201711-columbia-asia-b12-d-lipid-profile.pdf'),
+  ('November 2017 Metropolis Healthcare vitamin and TSH tests','2017-11-17','Blood',true,'Metropolis Healthcare','Self','Tests for Vitamin B12, Vitamin D3, and Thyroid Stimulating Hormone (TSH). The goal of the B12 and D3 tests was to validate and cross-check the low readings from the test done a few days before it at Columbia Asia','201711-metropolis-healthcare-b12-d-tsh.pdf'),
+  ('September 2018 LabCorp vitamin tests','2018-09-19','Blood',true,'LabCorp','Self (via Request A Test)','Tests for Vitamin B12 and Vitamin D3. The goal was to measure the effect of vitamin supplementation on vitamin levels after a few months of taking supplements. Prior to starting supplements, the last test (November 2017) had revealed low readings for both vitamins','201809-labcorp-b12-d.pdf');
---
+++ sql/tests.sql
@@ -0,0 +1,15 @@
+create table tests(
+  `test_slug` varchar(255) primary key not null,
+  `sample_type` enum('Blood','Stool'),
+  `fasting_required` boolean,
+  `labcorp_url` varchar(255),
+  `quest_diagnostics_url` varchar(255),
+  `typical_units` varchar(255),
+  `reference_interval_low_end` float,
+  `reference_interval_high_end` float,
+  `notes` varchar(2000)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+insert into tests(test_slug, labcorp_url, quest_diagnostics_url, typical_units, reference_interval_low_end, reference_interval_high_end, notes) values
+  ('D3','Blood',false,'https://www.labcorp.com/test-menu/36721/vitamin-d-25-hydroxy','https://www.questdiagnostics.com/testcenter/BUOrderInfo.action?tc=17306&labCode=AMD','ng/mL',30,100,'General range: < 10 ng/ML is deficient, 10-30 ng/mL is insufficient, 30-100 ng/mL is sufficient, > 100 ng/mL is toxic'),
+  ('B12','Blood',true,'https://www.labcorp.com/test-menu/36696/vitamin-bsub12-sub','https://www.questdiagnostics.com/testcenter/BUOrderInfo.action?tc=927&labCode=AMD','pg/mL',187,1245,'Different websites give different reference intervals for adults. Reference intervals have ranged from 187-885 on the low end, to 200-1100 (Quest Diagnostics) to 232-1245 (LabCorp)');
