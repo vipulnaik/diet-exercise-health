16d58ae416f105a9f1ef185d09ea909fb1760c8e
Vipul Naik <vipulnaik1@gmail.com>
2018-10-21 20:22:28 -0700

    Some syntax and format corrections discovered while trying to insert into SQL, for #1

--- sql/blood_tests.sql
+++ sql/blood_tests.sql
@@ -8,6 +8,6 @@ create table blood_tests(
   `notes` varchar(2000)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-insert into blood_tests(test_slug, labcorp_url, quest_diagnostics_url, medlineplus_url, typical_units, reference_interval_low_end, reference_interval_high_end, notes) values
+insert into blood_tests(test_slug, labcorp_url, quest_diagnostics_url, typical_units, reference_interval_low_end, reference_interval_high_end, notes) values
   ('D3','https://www.labcorp.com/test-menu/36721/vitamin-d-25-hydroxy','https://www.questdiagnostics.com/testcenter/BUOrderInfo.action?tc=17306&labCode=AMD','ng/mL',30,100,'General range: < 10 ng/ML is deficient, 10-30 ng/mL is insufficient, 30-100 ng/mL is sufficient, > 100 ng/mL is toxic'),
-  ('B12','https://www.labcorp.com/test-menu/36696/vitamin-bsub12-sub','https://www.questdiagnostics.com/testcenter/BUOrderInfo.action?tc=927&labCode=AMD','pg/mL',187,1245,'Different websites give different reference intervals. Reference intervals have ranged from 187-885 on the low end, to 200-1100 (Quest Diagnostics) to 232-1245 (LabCorp)');
+  ('B12','https://www.labcorp.com/test-menu/36696/vitamin-bsub12-sub','https://www.questdiagnostics.com/testcenter/BUOrderInfo.action?tc=927&labCode=AMD','pg/mL',187,1245,'Different websites give different reference intervals for adults. Reference intervals have ranged from 187-885 on the low end, to 200-1100 (Quest Diagnostics) to 232-1245 (LabCorp)');
