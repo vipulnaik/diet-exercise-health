7b4b3e457297540ea2c6ae7c0b09a6db4ea44995
Vipul Naik <vipulnaik1@gmail.com>
2021-01-18 20:15:24 -0800

    Add three historical cholesterol tests

--- sql/test_results.sql
+++ sql/test_results.sql
@@ -11,24 +11,42 @@ create table test_results(
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 insert into test_results(collection, test, units, reference_interval_low_end, reference_interval_high_end, measured_value, diagnosis, notes) values
-  # -- D3 tests
+  /* D3 tests */
   ('November 2017 Columbia Asia vitamin and lipid profile tests','D3','nmol/L',75,250,12.2,'Deficient','This test was taken before starting any regular Vitamin D3 supplementation'),
   ('November 2017 Metropolis Healthcare vitamin and TSH tests','D3','ng/mL',30,100,3,'Deficient','This test was taken before starting any regular Vitamin D3 supplementation'),
   ('September 2018 LabCorp vitamin tests','D3','ng/mL',30,100,35.8,'Sufficient','This test was taken after about 10 months of regular Vitamin D3 supplementation, with the supplement amount mostly 1000 IUs but occasionally going to 2000 IUs'),
-  # -- B12 tests
+  /* B12 tests */
   ('November 2017 Columbia Asia vitamin and lipid profile tests','B12','pg/mL',180,914,167,'Intermediate','This test was taken before starting any regular Vitamin B12 supplementation. Guidance says: < 145 pg/mL is deficient, 145-180 is intermediate, and 180-914 is normal'),
   ('November 2017 Metropolis Healthcare vitamin and TSH tests','B12','pg/mL',187,883,173.6,'Deficient','This test was taken before starting any regular Vitamin B12 supplementation'),
   ('September 2018 LabCorp vitamin tests','B12','pg/mL',232,1245,1050,'Sufficient','This test was taken after about 10 months of regular Vitamin B12 supplementation, at 1000 mcg per tablet and a little over 1 tablet per day on average'),
-  # -- Lipid profile test
+  /* Lipid profile tests: July 2006 SRL Bangalore */
+  ('July 2006 SRL Bangalore lipid profile tests','Cholesterol total','mg/dL',0,200,164,'Normal','This test was taken to provide a periodic measurement, in light of family diabetic tendencies'),
+  ('July 2006 SRL Bangalore lipid profile tests','Cholesterol HD direct','mg/dL',30,60,41.4,'Normal','This test was taken to provide a periodic measurement, in light of family diabetic tendencies'),
+  ('July 2006 SRL Bangalore lipid profile tests','Cholesterol LDL','mg/dL',0,100,110,'Above normal','This test was taken to provide a periodic measurement, in light of family diabetic tendencies'),
+  ('July 2006 SRL Bangalore lipid profile tests','Cholesterol VLDL','mg/dL',0,35,12.6,'Normal','This test was taken to provide a periodic measurement, in light of family diabetic tendencies'),
+  ('July 2006 SRL Bangalore lipid profile tests','Serum triglycerides','mg/dL',0,150,63,'Normal','This test was taken to provide a periodic measurement, in light of family diabetic tendencies'),
+  /* Lipid profile tests: July 2007 Gokula Metropolis */
+  ('July 2007 Gokula Metropolis lipid profile tests','Cholesterol total','mg/dL',0,200,145,'Normal','This test was taken to provide a periodic measurement, in light of family diabetic tendencies'),
+  ('July 2007 Gokula Metropolis lipid profile tests','Cholesterol HDL direct','mg/dL',30,60,35.6,'Normal','This test was taken to provide a periodic measurement, in light of family diabetic tendencies'),
+  ('July 2007 Gokula Metropolis lipid profile tests','Cholesterol LDL','mg/dL',0,100,105.4,'Above normal','This test was taken to provide a periodic measurement, in light of family diabetic tendencies'),
+  ('July 2007 Gokula Metropolis lipid profile tests','Cholesterol VLDL','mg/dL',0,35,53,'Above normal','This test was taken to provide a periodic measurement, in light of family diabetic tendencies'),
+  ('July 2007 Gokula Metropolis lipid profile tests','Serum triglycerides','mg/dL',0,150,265,'Above normal','This test was taken to provide a periodic measurement, in light of family diabetic tendencies'),
+  /* Lipid profile tests: July 2007 SRL Bangalore */
+  ('July 2007 SRL Bangalore lipid profile tests','Cholesterol total','mg/dL',0,200,156,'Normal','This test was taken to provide a periodic measurement, in light of family diabetic tendencies'),
+  ('July 2007 SRL Bangalore lipid profile tests','Cholesterol HDL direct','mg/dL',30,60,39,'Normal','This test was taken to provide a periodic measurement, in light of family diabetic tendencies'),
+  ('July 2007 SRL Bangalore lipid profile tests','Cholesterol LDL','mg/dL',0,100,90,'Normal','This test was taken to provide a periodic measurement, in light of family diabetic tendencies'),
+  ('July 2007 SRL Bangalore lipid profile tests','Cholesterol VLDL','mg/dL',0,35,26.6,'Normal','This test was taken to provide a periodic measurement, in light of family diabetic tendencies'),
+  ('July 2007 SRL Bangalore lipid profile tests','Serum triglycerides','mg/dL',0,150,133,'Normal','This test was taken to provide a periodic measurement, in light of family diabetic tendencies'),
+  /* Lipid purfile tests: November 2017 Columbia Asia */
   ('November 2017 Columbia Asia vitamin and lipid profile tests','Cholesterol total','mg/dL',0,200,168,'Normal','This test was taken to provide a periodic measurement, in light of family diabetic tendencies'),
   ('November 2017 Columbia Asia vitamin and lipid profile tests','Cholesterol HDL direct','mg/dL',30,60,51,'Normal','This test was taken to provide a periodic measurement, in light of family diabetic tendencies'),
   ('November 2017 Columbia Asia vitamin and lipid profile tests','Cholesterol LDL','mg/dL',0,100,109.7,'Above normal','This test was taken to provide a periodic measurement, in light of family diabetic tendencies'),
   ('November 2017 Columbia Asia vitamin and lipid profile tests','Cholesterol VLDL','mg/dL',0,35,15.8,'Normal','This test was taken to provide a periodic measurement, in light of family diabetic tendencies'),
   ('November 2017 Columbia Asia vitamin and lipid profile tests','Cholesterol HDL ratio','unitless',0,5,3.3,'Normal','This test was taken to provide a periodic measurement, in light of family diabetic tendencies'),
   ('November 2017 Columbia Asia vitamin and lipid profile tests','Serum triglycerides','mg/dL',0,150,79,'Normal','This test was taken to provide a periodic measurement, in light of family diabetic tendencies'),
-  # -- TSH
+  /* TSH */
   ('November 2017 Metropolis Healthcare vitamin and TSH tests','TSH','muIU/mL',0.35,4.94,2.30,'Sufficient','This test was taken to provide an initial measurement; there was no specific prior reason to believe that the readings would be out of range'),
-  # -- Gastro blood tests
+  /* Gastro blood tests */
   ('December 2016 LabCorp gastro blood tests','WBC','1000/muL',3.4,10.8,4.3,'Normal','The test was a part of a battery of gastro tests'),
   ('December 2016 LabCorp gastro blood tests','RBC','1000000/muL',4.14,5.80,5.19,'Normal','The test was a part of a battery of gastro tests'),
   ('December 2016 LabCorp gastro blood tests','Hemoglobin','g/dL',12.6,17.7,15.4,'Normal','The test was a part of a battery of gastro tests'),
