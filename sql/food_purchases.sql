96947bec928482325127a623d7c1e5f949e308c9
Vipul Naik <vipulnaik1@gmail.com>
2022-01-05 19:33:28 -0800

    Add Wednesday January 5 downtown Berkeley trip

--- sql/food_purchases.sql
+++ sql/food_purchases.sql
@@ -694,4 +694,16 @@ insert into food_purchases(purchase_date, food_type, quantity) values
   /* 2021-12-27 Udupi Palace */
   ('2021-12-27','Udupi Palace spinach masala dosa',1),
   /* 2021-12-29 Mehak Indian Cuisine; order at 6:04 PM */
-  ('2021-12-29','Mehal Indian Cuisine standard set',1);
+  ('2021-12-29','Mehal Indian Cuisine standard set',1),
+  /* 2022-01-05 Trader Joe'; checkout at 5:58 PM; total cost $34.80 (paid by card) */
+  ('2022-01-05','TJ Almond Milk',1),
+  ('2022-01-05','TJ Carrots',1),
+  ('2022-01-05','Alvarado Sprouted Wheat Tortillas',2),
+  ('2022-01-05','TJ Whole Wheat Tortillas',2),
+  ('2022-01-05','TJ Baby Spinach',1),
+  ('2022-01-05','Beefsteak tomato',7),
+  ('2022-01-05','Green bell pepper',2),
+  ('2022-01-05','Red bell pepper',2),
+  ('2022-01-05','Eggplant',2),
+  /* 2022-01-05 Udupi Palace */
+  ('2022-01-05','Udupi Palace spinach masala dosa',1);
