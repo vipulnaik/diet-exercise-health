58397683dd782f058a3159b709d77d3719deecaa
Vipul Naik <vipulnaik1@gmail.com>
2023-07-21 12:57:17 -0700

    Replace float -> decimal to address SQL warnings

--- sql/food_purchases.sql
+++ sql/food_purchases.sql
@@ -1,7 +1,7 @@
 create table food_purchases(
   `purchase_date` date not null,
   `food_type` varchar(200),
-  `quantity` float(11,2)
+  `quantity` decimal(11,2)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 insert into food_purchases(purchase_date, food_type, quantity) values
