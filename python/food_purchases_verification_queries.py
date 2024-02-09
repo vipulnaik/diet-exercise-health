3a93faf5ff8a4b265108ccad190b963aa71f8db0
Vipul Naik <vipulnaik1@gmail.com>
2024-02-09 00:42:22 -0800

    Add another food type; add verification queries to make sure that newly added food purchases have valid food types

--- python/food_purchases_verification_queries.py
+++ python/food_purchases_verification_queries.py
@@ -13,7 +13,23 @@ queries = [
    create temporary table purchased_items_with_frequencies_and_nutrition as
    select purchased_items_with_frequencies.*, food_types.*
    from purchased_items_with_frequencies left join food_types
-   on purchased_items_with_frequencies.food_type = food_types.short_name;""",
+   on purchased_items_with_frequencies.food_type = food_types.short_name;
+
+   drop temporary table if exists recently_purchased_items_with_frequencies;
+
+   create temporary table recently_purchased_items_with_frequencies as
+   select food_type, sum(quantity) as total_quantity, count(1) as freq
+   from food_purchases where datediff(curdate(), purchase_date) <= 91
+   group by food_type;
+
+   drop temporary table if exists recently_purchased_items_with_frequencies_and_nutrition;
+
+   create temporary table recently_purchased_items_with_frequencies_and_nutrition as
+   select recently_purchased_items_with_frequencies.*, food_types.*
+   from recently_purchased_items_with_frequencies left join food_types
+   on recently_purchased_items_with_frequencies.food_type = food_types.short_name;""",
+
+    "select * from recently_purchased_items_with_frequencies_and_nutrition where short_name is null;",
 ]
 
 _connection = connection.connect()
