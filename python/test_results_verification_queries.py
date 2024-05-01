c319bfb9909edc03293f8dd1247e9fb0c41408ff
Vipul Naik <vipulnaik1@gmail.com>
2024-05-01 15:11:55 -0700

    Reconcile and fix some units

--- python/test_results_verification_queries.py
+++ python/test_results_verification_queries.py
@@ -6,6 +6,8 @@ queries = [
     "select * from test_results where diagnosis = 'Normal' and (reference_interval_high_end < measured_value);",
 
     "select * from test_results where (reference_interval_low_end is null or reference_interval_low_end < measured_value) and (reference_interval_high_end is null or reference_interval_high_end > measured_value) and not (diagnosis in ('Normal','Sufficient','Negative'));",
+
+    "select test, count(distinct units) as num_distinct_units, group_concat(distinct units) from test_results group by test having num_distinct_units > 1 and not (test = 'D3' and num_distinct_units = 2);",
 ]
 
 _connection = connection.connect()
