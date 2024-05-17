c5befa753637a7a534bdce98020eafe132dba5f0
Vipul Naik <vipulnaik1@gmail.com>
2024-05-17 11:20:21 -0700

    Add verification queries for nutritional information for food types that check for consistency within a broad food type

--- python/food_types_verification_queries.py
+++ python/food_types_verification_queries.py
@@ -22,6 +22,23 @@ queries = [
     total_carb_in_grams = 0 and not (
       fiber_in_grams = 0 and sugars_in_grams = 0
     );""",
+
+    """
+    select broad_food_type,
+    coalesce(min(total_carb_in_grams / calories), 0) as carb_calorie_ratio_min,
+    coalesce(max(total_carb_in_grams / calories), 0) as carb_calorie_ratio_max
+    from food_types where broad_food_type is not null group by broad_food_type
+    having carb_calorie_ratio_max > 2 * carb_calorie_ratio_min + 0.005
+    and not (broad_food_type in ('Sauerkraut'));""",
+
+    """
+    select broad_food_type,
+    coalesce(min(total_fat_in_grams / calories), 0) as fat_calorie_ratio_min,
+    coalesce(max(total_fat_in_grams / calories), 0) as fat_calorie_ratio_max from food_types
+    where broad_food_type is not null
+    group by broad_food_type
+    having fat_calorie_ratio_max > 2 * fat_calorie_ratio_min + 0.01
+    and not (broad_food_type in ('Sauerkraut','Whole wheat tortilla'));""",
 ]
 
 _connection = connection.connect()
