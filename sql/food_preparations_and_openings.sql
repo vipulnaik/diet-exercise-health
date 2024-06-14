328793e2dabfd43af6d93079756bac81f6bad601
Vipul Naik <vipulnaik1@gmail.com>
2024-06-13 20:05:10 -0700

    Record opening of an almond milk bottle

--- sql/food_preparations_and_openings.sql
+++ sql/food_preparations_and_openings.sql
@@ -63,4 +63,6 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   /* Walnuts */
   ('2024-06-12','TJ Walnuts',1),
   /* Rice prep 2024-06-13 */
-  ('2024-06-13','Lundberg Sustainable California White Basmati Rice',0.75);
+  ('2024-06-13','Lundberg Sustainable California White Basmati Rice',0.75),
+  /* New bottle openings as the old ones finished */
+  ('2024-06-13','TJ Almond Milk',1);
