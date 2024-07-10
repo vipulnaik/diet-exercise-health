c6d97af9131fc460e495b94eb9cb1d78300292fa
Vipul Naik <vipulnaik1@gmail.com>
2024-07-09 20:43:33 -0700

    Record rice prep

--- sql/food_preparations_and_openings.sql
+++ sql/food_preparations_and_openings.sql
@@ -170,5 +170,7 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2024-07-09','Red bell pepper',2),
   ('2024-07-09','Beefsteak tomato',4),
   /* New bottle/packet openings as the old ones finished */
-  ('2024-07-09','TJ Miso Ginger Broth',1);
+  ('2024-07-09','TJ Miso Ginger Broth',1),
+  /* Rice prep 2024-07-09 */
+  ('2024-07-09','Lundberg Sustainable California White Basmati Rice',0.8);
   /* Double-check the date before committing */
