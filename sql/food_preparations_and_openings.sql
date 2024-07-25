a888b5d140c34b0415cffa25091140fd139e3e67
Vipul Naik <vipulnaik1@gmail.com>
2024-07-24 18:59:23 -0700

    Add two bottle openings

--- sql/food_preparations_and_openings.sql
+++ sql/food_preparations_and_openings.sql
@@ -233,5 +233,7 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2024-07-23','Beefsteak tomato',2),
   /* New bottle/packet openings as the old ones finished */
   ('2024-07-23','TJ Almond Milk',1),
-  ('2024-07-23','TJ Miso Ginger Broth',1);
+  ('2024-07-23','TJ Miso Ginger Broth',1),
+  ('2024-07-24','Kite Hill Greek Style Plant-Based Yogurt',1),
+  ('2024-07-24','TJ Walnuts',1);
   /* Double-check the date before committing */
