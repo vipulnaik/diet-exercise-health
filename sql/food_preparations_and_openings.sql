31fd5528c525dc6c9a6c013a7e4c7b148e86b3a6
Vipul Naik <vipulnaik1@gmail.com>
2024-07-30 09:18:47 -0700

    Record rice prep

--- sql/food_preparations_and_openings.sql
+++ sql/food_preparations_and_openings.sql
@@ -257,5 +257,7 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2024-07-29','TJ Carrots',1),
   ('2024-07-29','Green bell pepper',4),
   ('2024-07-29','Orange bell pepper',2),
-  ('2024-07-29','Beefsteak tomato',5);
+  ('2024-07-29','Beefsteak tomato',5),
+  /* Rice prep 2024-07-30 */
+  ('2024-07-30','Lundberg Sustainable California White Basmati Rice',0.8);
   /* Double-check the date before committing */
