05aaf224033a2d7bee2f18e96b12dcddf3692958
Vipul Naik <vipulnaik1@gmail.com>
2024-08-03 00:47:23 -0700

    Record rice prep

--- sql/food_preparations_and_openings.sql
+++ sql/food_preparations_and_openings.sql
@@ -264,5 +264,7 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2024-07-31','Kite Hill Greek Style Plant-Based Yogurt',1),
   ('2024-08-01','TJ Walnuts',1),
   /* Kitava dine-in */
-  ('2024-08-02','Kitava Power Bowl',1);
+  ('2024-08-02','Kitava Power Bowl',1),
+  /* Rice prep 2024-08-02 */
+  ('2024-08-02','Lundberg Sustainable California White Basmati Rice',0.8);
   /* Double-check the date before committing */
