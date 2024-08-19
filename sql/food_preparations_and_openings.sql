f0a579a738f961b5cdf00bcd167623faad04f786
Vipul Naik <vipulnaik1@gmail.com>
2024-08-18 18:19:19 -0700

    Record an opening

--- sql/food_preparations_and_openings.sql
+++ sql/food_preparations_and_openings.sql
@@ -330,5 +330,6 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2024-08-16','Lundberg Sustainable California White Basmati Rice',0.8),
   /* New bottle/packet openings as the old ones finished */
   ('2024-08-17','TJ Almond Milk',1),
-  ('2024-08-17','TJ Miso Ginger Broth',1);
+  ('2024-08-17','TJ Miso Ginger Broth',1),
+  ('2024-08-18','Kite Hill Greek Style Plant-Based Yogurt',1); /* first meal, previous piece had been fully finished yesterday */
   /* Double-check the date before committing */
