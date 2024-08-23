2b648e5aa335e0b7b6865166492056bc363ab126
Vipul Naik <vipulnaik1@gmail.com>
2024-08-23 14:10:01 -0700

    Record meal prep

--- sql/food_preparations_and_openings.sql
+++ sql/food_preparations_and_openings.sql
@@ -346,5 +346,12 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2024-08-21','TJ Walnuts',1),
   ('2024-08-22','TJ Almond Milk',1), /* opened with first meal */
   /* Rice prep 2024-08-22 */
-  ('2024-08-22','Lundberg Sustainable California White Basmati Rice',0.8);
+  ('2024-08-22','Lundberg Sustainable California White Basmati Rice',0.8),
+  /* Meal prep 2024-08-23 */
+  ('2024-08-23','Eggplant',1),
+  ('2024-08-23','TJ Kale',1),
+  ('2024-08-23','TJ Carrots',1),
+  ('2024-08-23','Green bell pepper',4),
+  ('2024-08-23','Orange bell pepper',2),
+  ('2024-08-23','Beefsteak tomato',6);
   /* Double-check the date before committing */
