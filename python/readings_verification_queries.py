dac1fb36f0327878887f97e86f3c9357ce418e66
Vipul Naik <vipulnaik1@gmail.com>
2024-08-27 08:32:29 -0700

    Add a pulse oximeter reading; mark it as an exception for the max heart rate verification query

--- python/readings_verification_queries.py
+++ python/readings_verification_queries.py
@@ -13,7 +13,7 @@ queries = [
     # Pulse oximeter
     "select * from pulse_oximeter_readings where not (within_an_hour_after_exercise or within_two_hours_after_eating) and not (heart_rate_min_bpm between 40 and 65);",
 
-    "select * from pulse_oximeter_readings where not (within_an_hour_after_exercise or within_two_hours_after_eating) and not (heart_rate_max_bpm between 45 and 70);",
+    "select * from pulse_oximeter_readings where not (within_an_hour_after_exercise or within_two_hours_after_eating) and not (heart_rate_max_bpm between 45 and 70) and not (reading_date = '2024-08-27' and index_within_reading_date = 1);",
 
     "select * from pulse_oximeter_readings where (within_an_hour_after_exercise or within_two_hours_after_eating) and not (heart_rate_min_bpm between 60 and 100);",
 
