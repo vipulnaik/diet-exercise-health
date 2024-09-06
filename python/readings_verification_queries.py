4593eeb6b5395d15841770087bd28cf928a74f8d
Vipul Naik <vipulnaik1@gmail.com>
2024-09-06 09:23:34 -0700

    Add a pulse oximeter reading; tweak a threshold

--- python/readings_verification_queries.py
+++ python/readings_verification_queries.py
@@ -19,7 +19,7 @@ queries = [
 
     "select * from pulse_oximeter_readings where (within_an_hour_after_exercise or within_two_hours_after_eating) and not (heart_rate_max_bpm between 65 and 105);",
 
-    "select * from pulse_oximeter_readings where not (spo2_min between 93 and 100);",
+    "select * from pulse_oximeter_readings where not (spo2_min between 92 and 100);",
 
     "select * from pulse_oximeter_readings where not (spo2_max between 96 and 100);",
 ]
