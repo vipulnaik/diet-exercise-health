0514d15f0f54753a4db6a6db8ca667ff931a68df
Vipul Naik <vipulnaik1@gmail.com>
2024-09-06 20:46:19 -0700

    Record two openings

--- sql/food_preparations_and_openings.sql
+++ sql/food_preparations_and_openings.sql
@@ -396,5 +396,8 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2024-09-04','Kite Hill Greek Style Plant-Based Yogurt',1),
   ('2024-09-06','TJ Walnuts',1), /* first meal */
   /* Rice prep 2024-09-06 (second meal) */
-  ('2024-09-06','Lundberg Sustainable California White Basmati Rice',0.8);
+  ('2024-09-06','Lundberg Sustainable California White Basmati Rice',0.8),
+  /* New bottle/packet openings as the old ones finished */
+  ('2024-09-06','TJ Almond Milk',1), /* technically opened with first meal but started using in second meal */
+  ('2024-09-06','TJ Miso Ginger Broth',1); /* opened with second meal */
   /* Double-check the date before committing */
