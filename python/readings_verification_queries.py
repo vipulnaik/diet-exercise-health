b32d71706cd018f4acbcf9eb45bafd842c0fa987
Vipul Naik <vipulnaik1@gmail.com>
2024-09-11 21:26:51 -0700

    Add a pulse oximeter reading and a breath test reading; tweak a verification query

--- python/readings_verification_queries.py
+++ python/readings_verification_queries.py
@@ -15,9 +15,9 @@ queries = [
 
     "select * from pulse_oximeter_readings where not (within_an_hour_after_exercise or within_two_hours_after_eating) and not (heart_rate_max_bpm between 45 and 70) and not (reading_date = '2024-08-27');",
 
-    "select * from pulse_oximeter_readings where (within_an_hour_after_exercise or within_two_hours_after_eating) and not (heart_rate_min_bpm between 60 and 100);",
+    "select * from pulse_oximeter_readings where (within_an_hour_after_exercise or within_two_hours_after_eating) and not (heart_rate_min_bpm between 50 and 100);",
 
-    "select * from pulse_oximeter_readings where (within_an_hour_after_exercise or within_two_hours_after_eating) and not (heart_rate_max_bpm between 65 and 105);",
+    "select * from pulse_oximeter_readings where (within_an_hour_after_exercise or within_two_hours_after_eating) and not (heart_rate_max_bpm between 55 and 105);",
 
     "select * from pulse_oximeter_readings where not (spo2_min between 92 and 100);",
 
