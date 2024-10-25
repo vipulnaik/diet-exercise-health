00b31b82ea86d37233c002917321ca78c236c77b
Vipul Naik <vipulnaik1@gmail.com>
2024-10-24 20:26:47 -0700

    Record potatoes prep and various openings (including one from yesterday)

--- sql/food_preparations_and_openings.sql
+++ sql/food_preparations_and_openings.sql
@@ -618,5 +618,15 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2024-10-22','Lundberg Sustainable California White Basmati Rice',0.8),
   ('2024-10-22','TJ Turmeric',1), /* opened as part of rice prep; coincidence that I opened a new cumin bottle earlier in the day for lentils prep! */
   /* New bottle/packet openings as the old ones finished */
-  ('2024-10-22','TJ Miso Ginger Broth',1); /* second meal */
+  ('2024-10-22','TJ Miso Ginger Broth',1), /* second meal */
+  ('2024-10-23','TJ Carb Savvy Tortillas',1), /* second meal */
+  /* Potatoes prep 2024-10-24 */
+  ('2024-10-24','Gold potatoes 5 lb',0.43),
+  ('2024-10-24','TJ Kale',1),
+  ('2024-10-24','TJ English Shelled Peas',2),
+  ('2024-10-24','Beefsteak tomato',6),
+  /* New bottle/packet openings as the old ones finished */
+  ('2024-10-24','TJ Roasted Seaweed',1), /* second meal */
+  ('2024-10-24','Kite Hill Greek Style Plant-Based Yogurt',1), /* second meal */
+  ('2024-10-24','TJ Carb Savvy Tortillas',1); /* second meal */
   /* Double-check the date before committing */
