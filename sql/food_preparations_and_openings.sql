f1d9e5bdbaf8e4271d8b5cf68a5fb075d9469650
Vipul Naik <vipulnaik1@gmail.com>
2024-11-23 15:54:27 -0800

    Record an opening

--- sql/food_preparations_and_openings.sql
+++ sql/food_preparations_and_openings.sql
@@ -788,5 +788,6 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2024-11-21','TJ Turmeric',1), /* prepared for use in potatoes prep above */
   ('2024-11-21','TJ Carb Savvy Tortillas',1), /* second meal */
   ('2024-11-22','Kite Hill Greek Style Plant-Based Yogurt',1), /* first meal */
-  ('2024-11-22','TJ Roasted Seaweed',1); /* second meal */
+  ('2024-11-22','TJ Roasted Seaweed',1), /* second meal */
+  ('2024-11-23','TJ Sauerkraut',1); /* first meal */
   /* Double-check the date before committing */
