01472525e22cea38a361791f50a30a4d5f71827c
Vipul Naik <vipulnaik1@gmail.com>
2024-12-15 15:49:37 -0800

    Record two openings

--- sql/food_preparations_and_openings_2024.sql
+++ sql/food_preparations_and_openings_2024.sql
@@ -912,5 +912,7 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2024-12-14','TJ English Shelled Peas',2,2),
   ('2024-12-14','Beefsteak tomato',7,2),
   /* New bottle/packet openings as the old ones finished */
-  ('2024-12-14','TJ Miso Ginger Broth',1,2);
+  ('2024-12-14','TJ Miso Ginger Broth',1,2),
+  ('2024-12-15','Kite Hill Greek Style Plant-Based Yogurt',1,1),
+  ('2024-12-15','California Walnuts',1,1);
   /* Double-check the date before committing */
