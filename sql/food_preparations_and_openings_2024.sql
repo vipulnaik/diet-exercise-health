066183a4f9f27c00c65981f2336723d1566f4073
Vipul Naik <vipulnaik1@gmail.com>
2024-12-18 21:43:47 -0800

    Record consumption of Udupi Palace spinach masala dosa

--- sql/food_preparations_and_openings_2024.sql
+++ sql/food_preparations_and_openings_2024.sql
@@ -931,5 +931,7 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2024-12-17','Lundberg Sustainable California White Basmati Rice',0.85,1),
   /* New bottle/packet openings as the old ones finished */
   ('2024-12-17','TJ Carb Savvy Tortillas',1,1),
-  ('2024-12-17','Kite Hill Greek Style Plant-Based Yogurt',1,1);
+  ('2024-12-17','Kite Hill Greek Style Plant-Based Yogurt',1,1),
+  /* Takeout food 2024-12-18 (second meal) -- I had the dosa fully in the meal but the sambhar and chutney only partially, in order to reduce the "outside food" load in a single day */
+  ('2024-12-18','Udupi Palace spinach masala dosa',1,2);
   /* Double-check the date before committing */
