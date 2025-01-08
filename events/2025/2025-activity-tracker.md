a6dde4ebb9b61e3a58bb222b787a5b9c5d85e7b8
Vipul Naik <vipulnaik1@gmail.com>
2025-01-07 21:45:29 -0800

    Add Tuesday 2025-01-07 downtown Berkeley trip

--- events/2025/2025-activity-tracker.md
+++ events/2025/2025-activity-tracker.md
@@ -5,3 +5,11 @@
 On Thursday 2025-01-02, I made a trip for administrative tasks,
 including a stop at the Amazon Hub Locker at ASUC Berkeley for package
 pickup.
+
+## 2025-01-07
+
+On Tuesday 2025-01-07, in the evening, I made a multi-stop trip in
+downtown Berkeley. I dropped off two used, worn-and-torn sweatshirts
+at Goodwill. I then went to Citibank to get quarters for
+laundry. Finally, I went to Trader Joe's for food purchases recorded
+[here](../../sql/food_purchases_2025.sql).
--- sql/food_purchases_2025.sql
+++ sql/food_purchases_2025.sql
@@ -9,7 +9,14 @@ insert into food_purchases(purchase_date, food_type, quantity) values
   ('2025-01-05','Beefsteak tomato',9),
   ('2025-01-05','TJ Carrots',1),
   ('2025-01-05','TJ Roasted Seaweed',1),
-  ('2025-01-05','California Walnuts',1); /* chopped ones this time */
+  ('2025-01-05','California Walnuts',1), /* chopped ones this time */
+  /* 2025-01-07 Trader Joe's; total cost $35.10; checkout t 4:56 PM; paid by debit card */
+  ('2025-01-07','Gold potatoes 3 lb',1),
+  ('2025-01-07','TJ Miso Ginger Broth',1),
+  ('2025-01-07','TJ English Shelled Peas',4),
+  ('2025-01-07','TJ Carb Savvy Tortillas',1),
+  ('2025-01-07','TJ Kale',1),
+  ('2025-01-07','Eggplant',2);
   /* after entering a batch of data for a purchase:
   (a) eyeball for correct format
   (b) double-check date and C-s search to confirm correctness
