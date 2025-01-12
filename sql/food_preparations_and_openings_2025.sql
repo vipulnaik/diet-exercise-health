b8dd5da0201e534d6098fbbc1099267306bede12
Vipul Naik <vipulnaik1@gmail.com>
2025-01-11 22:36:58 -0800

    Retroactively add an opening

--- sql/food_preparations_and_openings_2025.sql
+++ sql/food_preparations_and_openings_2025.sql
@@ -52,6 +52,7 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-01-08','Kite Hill Greek Style Plant-Based Yogurt',1,2),
   ('2025-01-10','TJ Miso Ginger Broth',1,2),
   ('2025-01-10','TJ Roasted Seaweed',1,2),
+  ('2025-01-10','California Walnuts',1,2), /* added retroactively, potentially a little off; chopped walnuts this time */
   /* 2025-01-10 lentils prep (overnight soaking to cook the next day); added tuermeric, cumin, and salt */
   ('2025-01-10','TJ Green Lentils',0.5,3),
   ('2025-01-11','TJ Turmeric',1,1), /* opened when cooking lentils soaked overnight */
