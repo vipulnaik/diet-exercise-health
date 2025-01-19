304b116207fb14c79b33ebc2e453bb903d9cf86b
Vipul Naik <vipulnaik1@gmail.com>
2025-01-18 20:05:59 -0800

    Add Saturday 2025-01-18 Trader Joe's trip

--- sql/food_purchases_2025.sql
+++ sql/food_purchases_2025.sql
@@ -38,7 +38,15 @@ insert into food_purchases(purchase_date, food_type, quantity) values
   ('2025-01-16','TJ Miso Ginger Broth',1),
   ('2025-01-16','TJ Carrots',1),
   ('2025-01-16','TJ English Shelled Peas',4),
-  ('2025-01-16','TJ Turmeric',1);
+  ('2025-01-16','TJ Turmeric',1),
+  /* 2025-01-18 Trader Joe's; total cost $31.93; checkout at 6:04 PM; paid by credit card; 17 items in transaction */
+  ('2025-01-18','TJ Kale',1),
+  ('2025-01-18','TJ Carb Savvy Tortillas',3),
+  ('2025-01-18','Green bell pepper',4),
+  ('2025-01-18','Eggplant',2),
+  ('2025-01-18','Beefsteak tomato',4),
+  ('2025-01-18','Gold potatoes 5 lb',1),
+  ('2025-01-18','Red bell pepper',2);
   /* after entering a batch of data for a purchase:
   (a) eyeball for correct format
   (b) double-check date and C-s search to confirm correctness
