37d7769c4032dc917a9930e4c77932dedf6934d4
Vipul Naik <vipulnaik1@gmail.com>
2025-02-24 20:58:53 -0800

    Record an opening

--- sql/food_preparations_and_openings_2025.sql
+++ sql/food_preparations_and_openings_2025.sql
@@ -318,5 +318,6 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-02-24','TJ English Shelled Peas',2,1),
   ('2025-02-24','Beefsteak tomato',5,1),
   /* New bottle/packet openings as the old ones finished */
-  ('2025-02-24','TJ Carb Savvy Tortillas',1,1);
+  ('2025-02-24','TJ Carb Savvy Tortillas',1,1),
+  ('2025-02-24','Kite Hill Greek Style Plant-Based Yogurt',1,2);
   /* Double-check the date before committing */
