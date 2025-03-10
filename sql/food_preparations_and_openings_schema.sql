e0483d4386934f7edfb7a6eb05a0ceeefffb0b7a
Vipul Naik <vipulnaik1@gmail.com>
2025-03-09 22:19:39 -0700

    Add date order checks on food preparations and opening table

--- sql/food_preparations_and_openings_schema.sql
+++ sql/food_preparations_and_openings_schema.sql
@@ -4,5 +4,7 @@ create table food_preparations_and_openings(
   `quantity` decimal(11,2),
   `meal_index` integer,
   `completion_date` date,
-  `completion_meal_index` integer
+  `completion_meal_index` integer,
+  check(preparation_or_opening_date <= completion_date),
+  check(preparation_or_opening_date < completion_date or meal_index <= completion_meal_index)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
