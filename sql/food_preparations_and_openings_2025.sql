6407e4526b090576b625182d90a498fca9301c67
Vipul Naik <vipulnaik1@gmail.com>
2025-03-12 12:34:05 -0700

    Record rice prep; record rice completion yesterday

--- sql/food_preparations_and_openings_2025.sql
+++ sql/food_preparations_and_openings_2025.sql
@@ -385,8 +385,8 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-03-08','Kite Hill Greek Style Plant-Based Yogurt',1,1,'2025-03-09',2),
   ('2025-03-08','TJ Carb Savvy Tortillas',1,1,'2025-03-09',1),
   /* 2025-03-09 rice prep (first meal) (mix of basmati rice and jasmine rice) (added turmeric, cumin, salt) */
-  ('2025-03-09','Lundberg Sustainable California White Basmati Rice',0.1,1,NULL,NULL),
-  ('2025-03-09','Lundberg Sustainable California White Jasmine Rice',0.8,1,NULL,NULL),
+  ('2025-03-09','Lundberg Sustainable California White Basmati Rice',0.1,1,'2025-03-11',2),
+  ('2025-03-09','Lundberg Sustainable California White Jasmine Rice',0.8,1,'2025-03-11',2),
   /* New bottle/packet openings as the old ones finished */
   ('2025-03-09','TJ Carb Savvy Tortillas',1,2,'2025-03-10',2),
   ('2025-03-09','Whole Foods Walnuts',1,2,NULL,NULL),
@@ -402,5 +402,7 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-03-10','TJ Cumin',1,1,NULL,NULL), /* opened with eggplant meal prep and also finished the old one in the same meal prep */
   ('2025-03-10','Kite Hill Greek Style Plant-Based Yogurt',1,1,'2025-03-11',2),
   ('2025-03-10','TJ Carb Savvy Tortillas',1,2,'2025-03-11',2),
-  ('2025-03-11','TJ Carb Savvy Tortillas',1,2,NULL,NULL);
+  ('2025-03-11','TJ Carb Savvy Tortillas',1,2,NULL,NULL),
+  /* 2025-03-12 rice prep (first meal) (added turmeric, cumin, salt) */
+  ('2025-03-12','Lundberg Sustainable California White Jasmine Rice',0.9,1,NULL,NULL);
   /* Double-check the date before committing */
