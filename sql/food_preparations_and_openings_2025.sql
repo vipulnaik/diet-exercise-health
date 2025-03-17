6a0bccd58db708e54c97f8bdc7a9333d04d59cc9
Vipul Naik <vipulnaik1@gmail.com>
2025-03-17 14:39:34 -0700

    Record an opening and associated completion

--- sql/food_preparations_and_openings_2025.sql
+++ sql/food_preparations_and_openings_2025.sql
@@ -435,5 +435,6 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-03-15','TJ Almond Milk',1,1,NULL,NULL),
   ('2025-03-15','Kite Hill Greek Style Plant-Based Yogurt',1,2,NULL,NULL),
   ('2025-03-16','TJ Miso Ginger Broth',1,1,NULL,NULL),
-  ('2025-03-16','TJ Carb Savvy Tortillas',1,1,NULL,NULL);
+  ('2025-03-16','TJ Carb Savvy Tortillas',1,1,'2025-03-17',1),
+  ('2025-03-17','TJ Carb Savvy Tortillas',1,1,NULL,NULL);
   /* Double-check the date before committing */
