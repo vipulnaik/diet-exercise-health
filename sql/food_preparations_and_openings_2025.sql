a02a407bd34bd48785704504cded9faded7b4698
Vipul Naik <vipulnaik1@gmail.com>
2025-03-26 11:14:24 -0700

    Record update regarding lentils prep

--- sql/food_preparations_and_openings_2025.sql
+++ sql/food_preparations_and_openings_2025.sql
@@ -481,6 +481,6 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-03-25','TJ Almond Milk',1,1,NULL,NULL),
   ('2025-03-25','TJ Walnuts',1,1,NULL,NULL),
   ('2025-03-25','Kite Hill Greek Style Plant-Based Yogurt',1,2,NULL,NULL),
-  /* 2025-03-25 lentils prep (overnight soaking to cook the next day) */
+  /* 2025-03-25 lentils prep (overnight soaking to cook the next day; added turmeric, cumin, and salt the next day (2025-03-26) after draining soaking water and adding water for cooking) */
   ('2025-03-25','TJ Green Lentils',0.5,3,NULL,NULL);
   /* Double-check the date before committing */
