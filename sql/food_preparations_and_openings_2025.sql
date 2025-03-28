3b89503b3692d67e66546fea8df23e3957e80aa4
Vipul Naik <vipulnaik1@gmail.com>
2025-03-27 23:02:34 -0700

    Record rice completion

--- sql/food_preparations_and_openings_2025.sql
+++ sql/food_preparations_and_openings_2025.sql
@@ -475,7 +475,7 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   /* New bottle/packet openings as the old ones finished */
   ('2025-03-23','Kite Hill Greek Style Plant-Based Yogurt',1,2,'2025-03-25',1),
   /* 2025-03-24 rice prep (second meal) (added turmeric, cumin, salt) */
-  ('2025-03-24','Lundberg Sustainable California White Basmati Rice',0.9,2,NULL,NULL),
+  ('2025-03-24','Lundberg Sustainable California White Basmati Rice',0.9,2,'2025-03-27',2),
   /* New bottle/packet openings as the old ones finished */
   ('2025-03-24','TJ Roasted Seaweed',1,2,NULL,NULL),
   ('2025-03-25','TJ Almond Milk',1,1,NULL,NULL),
