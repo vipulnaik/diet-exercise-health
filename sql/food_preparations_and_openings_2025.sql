9b313b9efa807b88b90f697e93f4093601b3d057
Vipul Naik <vipulnaik1@gmail.com>
2025-04-21 11:17:57 -0700

    Record rice prep; adjust amounts for previous rice preps

--- sql/food_preparations_and_openings_2025.sql
+++ sql/food_preparations_and_openings_2025.sql
@@ -581,7 +581,7 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-04-14','Kite Hill Greek Style Plant-Based Yogurt',1,1,'2025-04-15',2),
   /* 2025-04-14 rice prep (second meal) (added turmeric, cumin, salt) */
   ('2025-04-14','Lundberg Sustainable California White Basmati Rice',0.7,2,'2025-04-17',1),
-  ('2025-04-14','Lundberg Sustainable California White Jasmine Rice',0.2,2,'2025-04-17',1),
+  ('2025-04-14','Lundberg Sustainable California White Jasmine Rice',0.25,2,'2025-04-17',1),
   /* New bottle/packet openings as the old ones finished */
   ('2025-04-14','TJ Turmeric',1,2,NULL,NULL), /* opened with rice prep; used previous turmeric partially as well */
   ('2025-04-15','TJ Almond Milk',1,1,NULL,NULL),
@@ -599,7 +599,7 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-04-16','TJ Carb Savvy Tortillas',1,2,'2025-04-17',1),
   ('2025-04-16','TJ Roasted Seaweed',1,2,NULL,NULL),
   /* 2025-04-17 rice prep (second meal) (added turmeric, cumin, salt) */
-  ('2025-04-17','Lundberg Sustainable California White Jasmine Rice',0.9,2,'2025-04-20',2),
+  ('2025-04-17','Lundberg Sustainable California White Jasmine Rice',0.95,2,'2025-04-20',2),
   /* New bottle/packet openings as the old ones finished */
   ('2025-04-17','TJ Carb Savvy Tortillas',1,2,'2025-04-18',2),
   ('2025-04-17','Kite Hill Greek Style Plant-Based Yogurt',1,2,'2025-04-19',1),
@@ -612,5 +612,8 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-04-19','TJ English Shelled Peas',2,2,NULL,NULL),
   ('2025-04-19','Beefsteak tomato',5,2,NULL,NULL),
   /* New bottle/packet openings as the old ones finished */
-  ('2025-04-20','Udupi Palace spinach masala dosa free red chutney',1,1,NULL,NULL);
+  ('2025-04-20','Udupi Palace spinach masala dosa free red chutney',1,1,NULL,NULL),
+  /* 2025-04-21 rice prep (first meal) (added turmeric, cumin, salt) */
+  ('2025-04-21','Lundberg Sustainable California White Jasmine Rice',0.80,1,NULL,NULL),
+  ('2025-04-21','Lundberg Sustainable California White Basmatic Rice',0.15,1,NULL,NULL);
   /* Double-check the date before committing */
