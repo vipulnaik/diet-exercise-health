c3ab066dbc7916b84386d73bec1c1c5f64abe401
Vipul Naik <vipulnaik1@gmail.com>
2025-04-30 19:12:39 -0700

    Record three completions and one opening

--- sql/food_preparations_and_openings_2025.sql
+++ sql/food_preparations_and_openings_2025.sql
@@ -620,7 +620,7 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   /* New bottle/packet openings as the old ones finished */
   ('2025-04-21','Kite Hill Greek Style Plant-Based Yogurt',1,1,'2025-04-23',1),
   ('2025-04-22','TJ Carb Savvy Tortillas',1,1,'2025-04-23',1),
-  ('2025-04-22','TJ Walnuts',1,2,NULL,NULL),
+  ('2025-04-22','TJ Walnuts',1,2,'2025-04-30',2),
   ('2025-04-22','TJ Almond Milk',1,2,NULL,NULL),
   ('2025-04-23','Kite Hill Greek Style Plant-Based Yogurt',1,2,'2025-04-25',2),
   ('2025-04-23','TJ Miso Ginger Broth',1,2,NULL,NULL),
@@ -649,9 +649,10 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-04-27','TJ Carb Savvy Tortillas',1,1,'2025-04-29',1),
   ('2025-04-27','Kite Hill Greek Style Plant-Based Yogurt',1,2,'2025-04-29',1),
   /* 2025-04-27 lentils prep (overnight soaking to cook the next day; added turmeric, cumin, and salt the next day (2025-04-28) after draining soaking water and adding water for cooking) */
-  ('2025-04-27','TJ Green Lentils',0.5,3,NULL,NULL),
+  ('2025-04-27','TJ Green Lentils',0.5,3,'2025-04-30',2),
   /* 2025-04-28 rice prep (second meal) (added turmeric, cumin, salt) */
   ('2025-04-28','Lundberg Sustainable California White Basmati Rice',0.9,2,NULL,NULL),
   /* New bottle/packet openings as the old ones finished */
-  ('2025-04-29','Kite Hill Greek Style Plant-Based Yogurt',1,1,NULL,NULL);
+  ('2025-04-29','Kite Hill Greek Style Plant-Based Yogurt',1,1,'2025-04-30',2),
+  ('2025-04-30','TJ Walnuts',1,2,NULL,NULL);
   /* Double-check the date before committing */
