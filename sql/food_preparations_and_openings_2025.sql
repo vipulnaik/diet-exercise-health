46962d29d22b52ab43060ebcedf9296d48a4ff62
Vipul Naik <vipulnaik1@gmail.com>
2025-07-16 22:07:23 -0700

    Record a completion

--- sql/food_preparations_and_openings_2025.sql
+++ sql/food_preparations_and_openings_2025.sql
@@ -907,5 +907,5 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-07-13','TJ Carb Savvy Tortillas',1,2,'2025-07-15',1),
   ('2025-07-14','TJ Walnuts',1,2,null,null),
   ('2025-07-15','Kite Hill Greek Style Plant-Based Yogurt',1,1,null,null),
-  ('2025-07-15','TJ Carb Savvy Tortillas',1,2,null,null);
+  ('2025-07-15','TJ Carb Savvy Tortillas',1,2,'2025-07-16',2);
   /* Double-check the date before committing */
