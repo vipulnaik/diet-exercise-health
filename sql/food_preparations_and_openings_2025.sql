56e43e95bbe2bb1eb8ad4c9372500be92704b0c7
Vipul Naik <vipulnaik1@gmail.com>
2025-07-18 14:41:00 -0700

    Record a completion

--- sql/food_preparations_and_openings_2025.sql
+++ sql/food_preparations_and_openings_2025.sql
@@ -916,7 +916,7 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-07-17','Red bell pepper',2,1,null,null),
   ('2025-07-17','Beefsteak tomato',8,1,null,null),
   /* new bottle/packet openings as the old ones finished */
-  ('2025-07-17','TJ Carb Savvy Tortillas',1,1,null,null),
+  ('2025-07-17','TJ Carb Savvy Tortillas',1,1,'2025-07-18',1),
   ('2025-07-17','TJ Miso Ginger Broth',1,1,null,null),
   ('2025-07-17','Kite Hill Greek Style Plant-Based Yogurt',1,1,null,null),
   /* 2025-07-17 rice prep (second meal) (added turmeric, cumin, salt) */
