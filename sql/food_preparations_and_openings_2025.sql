7a63dab534f2843d0fcb110200229a828c7441e5
Vipul Naik <vipulnaik1@gmail.com>
2025-07-17 17:52:13 -0700

    Record eggplant meal prep, two completions, and three openings

--- sql/food_preparations_and_openings_2025.sql
+++ sql/food_preparations_and_openings_2025.sql
@@ -890,7 +890,7 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-07-09','TJ Carb Savvy Tortillas',1,2,'2025-07-11',2),
   ('2025-07-09','Kite Hill Greek Style Plant-Based Yogurt',1,2,'2025-07-12',1),
   ('2025-07-10','TJ Walnuts',1,1,'2025-07-14',1),
-  ('2025-07-10','TJ Miso Ginger Broth',1,1,null,null),
+  ('2025-07-10','TJ Miso Ginger Broth',1,1,'2025-07-17',1),
   ('2025-07-10','TJ Roasted Seaweed',1,2,null,null),
   /* 2025-07-11 potatoes prep (second meal) (added olive oil, turmeric, cumin, cayenne pepper, and salt; unusual features: (a) no peas, so more tomatoes than usual, (b) russet potatoes instead of gold potatoes) */
   ('2025-07-11','Russet potatoes 5 lb',0.5,2,null,null),
@@ -906,6 +906,17 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   /* new bottle/packet openings as the old ones finished */
   ('2025-07-13','TJ Carb Savvy Tortillas',1,2,'2025-07-15',1),
   ('2025-07-14','TJ Walnuts',1,2,null,null),
-  ('2025-07-15','Kite Hill Greek Style Plant-Based Yogurt',1,1,null,null),
-  ('2025-07-15','TJ Carb Savvy Tortillas',1,2,'2025-07-16',2);
+  ('2025-07-15','Kite Hill Greek Style Plant-Based Yogurt',1,1,'2025-07-17',1),
+  ('2025-07-15','TJ Carb Savvy Tortillas',1,2,'2025-07-16',2),
+  /* 2025-07-17 eggplant meal prep (first meal) (added olive oil, turmeric, cumin, salt, and a bit of cayenne pepper) */
+  ('2025-07-17','Eggplant',1,1,null,null),
+  ('2025-07-17','TJ Kale',1,1,null,null),
+  ('2025-07-17','TJ Carrots',1,1,null,null),
+  ('2025-07-17','Green bell pepper',4,1,null,null),
+  ('2025-07-17','Red bell pepper',2,1,null,null),
+  ('2025-07-17','Beefsteak tomato',8,1,null,null),
+  /* new bottle/packet openings as the old ones finished */
+  ('2025-07-17','TJ Carb Savvy Tortillas',1,1,null,null),
+  ('2025-07-17','TJ Miso Ginger Broth',1,1,null,null),
+  ('2025-07-17','Kite Hill Greek Style Plant-Based Yogurt',1,1,null,null);
   /* Double-check the date before committing */
