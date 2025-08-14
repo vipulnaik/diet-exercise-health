f6082849712e13dfc5092ce4f60f29860c4a9e13
Vipul Naik <vipulnaik1@gmail.com>
2025-08-13 18:58:39 -0700

    Record rice prep completion and two other completions and associated openings

--- sql/food_preparations_and_openings_2025.sql
+++ sql/food_preparations_and_openings_2025.sql
@@ -1005,14 +1005,14 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-08-07','TJ Carb Savvy Tortillas',1,2,'2025-08-10',1),
   ('2025-08-08','TJ Miso Ginger Broth',1,1,null,null),
   ('2025-08-08','TJ Roasted Seaweed',1,2,null,null),
-  ('2025-08-08','TJ Walnuts',1,2,null,null),
+  ('2025-08-08','TJ Walnuts',1,2,'2025-08-13',2),
   ('2025-08-08','Kite Hill Greek Style Plant-Based Yogurt',1,2,'2025-08-11',1),
   /* 2025-08-09 potatoes prep (first meal) (added olive oil, turmeric, cumin, cinnamon, cayenne pepper, and salt; unusual features: no peas) */
   ('2025-08-09','Gold potatoes 5 lb',0.5,1,null,null),
   ('2025-08-09','TJ Kale',1,1,null,null),
   ('2025-08-09','Beefsteak tomato',6,1,null,null),
   /* 2025-08-09 rice prep (second meal) (added turmeric, cumin, salt) */
-  ('2025-08-09','Lundberg Sustainable California White Basmati Rice',0.95,2,null,null),
+  ('2025-08-09','Lundberg Sustainable California White Basmati Rice',0.95,2,'2025-08-13',2),
   /* new bottle/packet openings as the old ones finished */
   ('2025-08-10','TJ Carb Savvy Tortillas',1,1,'2025-08-11',2),
   /* 2025-08-10 eggplant meal prep (second meal) (added olive oil, turmeric, cumin, cinnamon, salt, and a bit of cayenne pepper) */
@@ -1024,5 +1024,7 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-08-10','Beefsteak tomato',8,2,null,null),
   /* new bottle/packet openings as the old ones finished */
   ('2025-08-11','Kite Hill Greek Style Plant-Based Yogurt',1,2,null,null),
-  ('2025-08-12','TJ Carb Savvy Tortillas',1,1,null,null);
+  ('2025-08-12','TJ Carb Savvy Tortillas',1,1,'2025-08-13',2),
+  ('2025-08-13','TJ Walnuts',1,2,null,null),
+  ('2025-08-13','TJ Carb Savvy Tortillas',1,2,null,null);
   /* Double-check the date and meal index before committing */
