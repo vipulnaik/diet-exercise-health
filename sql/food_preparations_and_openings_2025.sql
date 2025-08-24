2c6972cf640d0af72e014e873ba1515452f0f074
Vipul Naik <vipulnaik1@gmail.com>
2025-08-24 15:11:01 -0700

    Record two completions and associated openings

--- sql/food_preparations_and_openings_2025.sql
+++ sql/food_preparations_and_openings_2025.sql
@@ -1039,7 +1039,7 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-08-15','Beefsteak tomato',4,2,'2025-08-23',1),
   /* new bottle/packet openings as the old ones finished */
   ('2025-08-15','TJ Roasted Seaweed',1,2,'2025-08-22',2),
-  ('2025-08-16','TJ Almond Milk',1,1,null,null),
+  ('2025-08-16','TJ Almond Milk',1,1,'2025-08-24',1),
   ('2025-08-16','TJ Carb Savvy Tortillas',1,1,'2025-08-17',2),
   ('2025-08-17','TJ Sauerkraut',1,1,null,null),
   ('2025-08-17','Kite Hill Greek Style Plant-Based Yogurt',1,1,'2025-08-19',2),
@@ -1062,7 +1062,7 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-08-22','Kite Hill Greek Style Plant-Based Yogurt',1,1,null,null),
   ('2025-08-22','TJ Miso Ginger Broth',1,1,null,null),
   ('2025-08-22','TJ Roasted Seaweed',1,2,null,null),
-  ('2025-08-23','TJ Carb Savvy Tortillas',1,1,null,null),
+  ('2025-08-23','TJ Carb Savvy Tortillas',1,1,'2025-08-24',1),
   /* 2025-08-23 potatoes prep (second meal) (added olive oil, turmeric, cumin, cinnamon, cayenne pepper, and salt) */
   ('2025-08-23','Gold potatoes 5 lb',0.5,2,null,null),
   ('2025-08-23','TJ Kale',1,2,null,null),
@@ -1072,5 +1072,8 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-08-23','TJ Turmeric',1,2,null,null), /* opened with potatoes prep; used previous turmeric partially as well */
   ('2025-08-23','TJ Walnuts',1,2,null,null),
   /* 2025-08-24 rice prep (first meal) (added turmeric, cumin, salt) */
-  ('2025-08-24','Lundberg Sustainable California White Basmati Rice',0.96,1,null,null);
+  ('2025-08-24','Lundberg Sustainable California White Basmati Rice',0.96,1,null,null),
+  /* new bottle/packet openings as the old ones finished */
+  ('2025-08-24','TJ Carb Savvy Tortillas',1,1,null,null),
+  ('2025-08-24','TJ Almond Milk',1,1,null,null);
   /* Double-check the date and meal index before committing */
