df00ed545bd01944931704f3c917b6db420df834
Vipul Naik <vipulnaik1@gmail.com>
2025-08-29 22:16:04 -0700

    Record eggplant meal prep, a completion, and two openings

--- sql/food_preparations_and_openings_2025.sql
+++ sql/food_preparations_and_openings_2025.sql
@@ -1077,11 +1077,21 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-08-24','TJ Carb Savvy Tortillas',1,1,'2025-08-26',1),
   ('2025-08-24','TJ Almond Milk',1,1,null,null),
   ('2025-08-25','Kite Hill Greek Style Plant-Based Yogurt',1,1,'2025-08-27',2),
-  ('2025-08-26','TJ Carb Savvy Tortillas',1,2,null,null),
+  ('2025-08-26','TJ Carb Savvy Tortillas',1,2,'2025-08-29',2),
   ('2025-08-27','TJ Miso Ginger Broth',1,2,null,null),
   /* 2025-08-28 rice prep (first meal) (added turmeric, cumin, salt) */
   ('2025-08-28','Lundberg Sustainable California White Jasmine Rice',0.96,1,null,null),
   /* new bottle/packet openings as the old ones finished */
   ('2025-08-28','Kite Hill Greek Style Plant-Based Yogurt',1,2,null,null), /* one meal in between where I didn't have the yogurt due to not having it in stock */
-  ('2025-08-29','TJ Walnuts',1,1,null,null);
+  ('2025-08-29','TJ Walnuts',1,1,null,null),
+  /* 2025-08-29 eggplant meal prep (second meal) (added olive oil, turmeric, cumin, cinnamon, salt, and a bit of cayenne pepper) */
+  ('2025-08-29','Eggplant',1,2,null,null),
+  ('2025-08-29','TJ Kale',1,2,null,null),
+  ('2025-08-29','TJ Carrots',1,2,null,null),
+  ('2025-08-29','Green bell pepper',4,2,null,null),
+  ('2025-08-29','Red bell pepper',2,2,null,null),
+  ('2025-08-29','Beefsteak tomato',8,2,null,null),
+  /* new bottle/packet openings as the old ones finished */
+  ('2025-08-29','TJ Carb Savvy Tortillas',1,2,null,null),
+  ('2025-08-29','TJ Roasted Seaweed',1,2,null,null);
   /* Double-check the date and meal index before committing */
