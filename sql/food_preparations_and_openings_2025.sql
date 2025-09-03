5790f1d4ac74e846a73357356ced37a2992c2c79
Vipul Naik <vipulnaik1@gmail.com>
2025-09-02 20:40:03 -0700

    Record a completion and associated opening

--- sql/food_preparations_and_openings_2025.sql
+++ sql/food_preparations_and_openings_2025.sql
@@ -1083,7 +1083,7 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-08-28','Lundberg Sustainable California White Jasmine Rice',0.96,1,'2025-09-01',1),
   /* new bottle/packet openings as the old ones finished */
   ('2025-08-28','Kite Hill Greek Style Plant-Based Yogurt',1,2,'2025-08-30',2), /* one meal in between where I didn't have the yogurt due to not having it in stock */
-  ('2025-08-29','TJ Walnuts',1,1,null,null),
+  ('2025-08-29','TJ Walnuts',1,1,'2025-09-02',2),
   /* 2025-08-29 eggplant meal prep (second meal) (added olive oil, turmeric, cumin, cinnamon, salt, and a bit of cayenne pepper; finished olive oil bottle with this prep and did not open a new one) */
   ('2025-08-29','Eggplant',1,2,null,null),
   ('2025-08-29','TJ Kale',1,2,null,null),
@@ -1108,5 +1108,6 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-09-01','Lundberg Sustainable California White Jasmine Rice',0.96,2,null,null),
   /* new bottle/packet openings as the old ones finished */
   ('2025-09-02','TJ Carb Savvy Tortillas',1,1,null,null),
-  ('2025-09-02','Kite Hill Greek Style Plant-Based Yogurt',1,1,null,null);
+  ('2025-09-02','Kite Hill Greek Style Plant-Based Yogurt',1,1,null,null),
+  ('2025-09-02','TJ Walnuts',1,2,null,null);
   /* Double-check the date and meal index before committing */
