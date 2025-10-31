6c41cc324fbc993688df91527d73a41d627232ed
Vipul Naik <vipulnaik1@gmail.com>
2025-10-30 22:21:47 -0700

    Record a completion and associated opening

--- sql/food_preparations_and_openings_2025.sql
+++ sql/food_preparations_and_openings_2025.sql
@@ -1348,12 +1348,13 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-10-28','Lundberg Sustainable California White Basmati Rice',0.95,2,null,null),
   /* new bottle/packet openings as the old ones finished */
   ('2025-10-29','Kite Hill Unsweetened Plain Almond Milk Yogurt',1,2,null,null),
-  ('2025-10-29','TJ Carb Savvy Tortillas',1,2,null,null),
+  ('2025-10-29','TJ Carb Savvy Tortillas',1,2,'2025-10-30',2),
   /* 2025-10-30 potatoes prep (first meal) (added olive oil, turmeric, cumin, cinnamon, cayenne pepper, and salt -- cayenne pepper resumed after more than a week past dental cleaning) */
   ('2025-10-30','Gold potatoes 5 lb',0.5,1,null,null),
   ('2025-10-30','TJ Kale',1,1,null,null),
   ('2025-10-30','TJ English Shelled Peas',2,1,null,null),
   ('2025-10-30','Beefsteak tomato',6,1,null,null),
   /* new bottle/packet openings as the old ones finished */
-  ('2025-10-30','TJ Miso Ginger Broth',1,1,null,null);
+  ('2025-10-30','TJ Miso Ginger Broth',1,1,null,null),
+  ('2025-10-30','TJ Carb Savvy Tortillas',1,2,null,null);
   /* Double-check the date and meal index before committing */
