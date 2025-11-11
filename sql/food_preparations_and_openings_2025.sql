fd464a8860863995ee71f99c8c9107e3885a59c9
Vipul Naik <vipulnaik1@gmail.com>
2025-11-11 00:27:28 -0800

    Record a completion

--- sql/food_preparations_and_openings_2025.sql
+++ sql/food_preparations_and_openings_2025.sql
@@ -1394,7 +1394,7 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   /* new bottle/packet openings as the old ones finished */
   ('2025-11-07','TJ Carb Savvy Tortillas',1,2,'2025-11-09',1),
   ('2025-11-08','Kite Hill Greek Style Plant-Based Yogurt',1,1,'2025-11-10',1),
-  ('2025-11-09','TJ Carb Savvy Tortillas',1,1,null,null),
+  ('2025-11-09','TJ Carb Savvy Tortillas',1,1,'2025-11-10',2),
   /* 2025-11-09 rice prep (second meal) (added turmeric, cumin, salt) */
   ('2025-11-09','Lundberg Sustainable California White Basmati Rice',0.95,2,null,null),
   /* new bottle/packet openings as the old ones finished */
