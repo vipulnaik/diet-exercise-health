8dc46f4b720a23e6f831e1cd98ee58c724805666
Vipul Naik <vipulnaik1@gmail.com>
2025-11-25 17:37:48 -0800

    Record rice prep

--- sql/food_preparations_and_openings_2025.sql
+++ sql/food_preparations_and_openings_2025.sql
@@ -1470,5 +1470,7 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-11-24','Beefsteak tomato',6,2,null,null),
   /* new bottle/packet openings as the old ones finished */
   ('2025-11-24','TJ Carb Savvy Tortillas',1,2,null,null),
-  ('2025-11-25','TJ Walnuts',1,1,null,null);
+  ('2025-11-25','TJ Walnuts',1,1,null,null),
+  /* 2025-11-25 rice prep (second meal) (added turmeric, cumin, salt) */
+  ('2025-11-25','Lundberg Sustainable California White Basmati Rice',0.95,2,null,null);
   /* Double-check the date and meal index before committing */
