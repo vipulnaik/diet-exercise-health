e95c0ffbb821dbb493cf3aa9adc450af6c032b07
Vipul Naik <vipulnaik1@gmail.com>
2025-12-06 20:41:20 -0800

    Record an opening and associated completion

--- sql/food_preparations_and_openings_2025.sql
+++ sql/food_preparations_and_openings_2025.sql
@@ -1523,5 +1523,6 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-12-06','Red bell pepper',2,1,null,null),
   ('2025-12-06','Beefsteak tomato',7,1,null,null),
   /* new bottle/packet openings as the old ones finished */
-  ('2025-12-06','TJ Sauerkraut',1,1,null,null);
+  ('2025-12-06','TJ Sauerkraut',1,1,null,null),
+  ('2025-12-06','TJ Carb Savvy Tortillas',1,2,'2025-12-06',2);
   /* Double-check the date and meal index before committing */
