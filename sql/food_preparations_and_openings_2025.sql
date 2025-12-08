5543fc94402510175d24bf137b1a35bc9fe72eb3
Vipul Naik <vipulnaik1@gmail.com>
2025-12-08 15:19:59 -0800

    Record potatoes prep as well as a completion and associated opening; add an exception to a verification query

--- sql/food_preparations_and_openings_2025.sql
+++ sql/food_preparations_and_openings_2025.sql
@@ -1530,5 +1530,12 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   /* new bottle/packet openings as the old ones finished */
   ('2025-12-07','Kite Hill Greek Style Plant-Based Yogurt',1,1,null,null),
   ('2025-12-07','TJ Walnuts',1,1,null,null),
-  ('2025-12-07','TJ Carb Savvy Tortillas',1,1,null,null);
+  ('2025-12-07','TJ Carb Savvy Tortillas',1,1,'2025-12-08',1),
+  /* 2025-12-08 potatoes prep (first meal) (added olive oil, turmeric, cumin, cinnamon, cayenne pepper, and salt) */
+  ('2025-12-08','Gold potatoes 5 lb',0.5,1,null,null),
+  ('2025-12-08','TJ Kale',1,1,null,null),
+  ('2025-12-08','TJ English Shelled Peas',2,1,null,null),
+  ('2025-12-08','Beefsteak tomato',6,1,null,null),
+  /* new bottle/packet openings as the old ones finished */
+  ('2025-12-08','TJ Carb Savvy Tortillas',1,1,null,null);
   /* Double-check the date and meal index before committing */
