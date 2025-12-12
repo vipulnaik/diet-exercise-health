05b4bb71535e39e8a795bfc7164028c1ae30a3ce
Vipul Naik <vipulnaik1@gmail.com>
2025-12-11 21:10:49 -0800

    Record two openings

--- sql/food_preparations_and_openings_2025.sql
+++ sql/food_preparations_and_openings_2025.sql
@@ -1549,5 +1549,7 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   /* new bottle/packet openings as the old ones finished */
   ('2025-12-10','Kite Hill Greek Style Plant-Based Yogurt',1,2,null,null),
   ('2025-12-11','TJ Miso Ginger Broth',1,1,null,null),
-  ('2025-12-11','Udupi Palace spinach masala dosa free red chutney',1,1,null,null);
+  ('2025-12-11','Udupi Palace spinach masala dosa free red chutney',1,1,null,null),
+  ('2025-12-11','TJ Carb Savvy Tortillas',1,2,null,null),
+  ('2025-12-11','TJ Walnuts',1,2,null,null);
   /* Double-check the date and meal index before committing */
