59941140b003ad4b24b92136d6b0ea18a0430d2f
Vipul Naik <vipulnaik1@gmail.com>
2025-12-20 16:35:34 -0800

    Record potatoes prep completion

--- sql/food_preparations_and_openings_2025.sql
+++ sql/food_preparations_and_openings_2025.sql
@@ -1564,10 +1564,10 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2025-12-14','TJ Carb Savvy Tortillas',1,2,'2025-12-15',2),
   ('2025-12-14','Kite Hill Greek Style Plant-Based Yogurt',1,2,'2025-12-15',2),
   /* 2025-12-15 potatoes prep (first meal) (added olive oil, turmeric, cumin, cinnamon, and salt; skipped cayenne pepper in light of recent Udupi Palace spinach masala dosa consumption) */
-  ('2025-12-15','Gold potatoes 5 lb',0.5,1,null,null),
-  ('2025-12-15','TJ Kale',1,1,null,null),
-  ('2025-12-15','TJ English Shelled Peas',2,1,null,null),
-  ('2025-12-15','Beefsteak tomato',4,1,null,null),
+  ('2025-12-15','Gold potatoes 5 lb',0.5,1,'2025-12-20',1),
+  ('2025-12-15','TJ Kale',1,1,'2025-12-20',1),
+  ('2025-12-15','TJ English Shelled Peas',2,1,'2025-12-20',1),
+  ('2025-12-15','Beefsteak tomato',4,1,'2025-12-20',1),
   /* new bottle/packet openings as the old ones finished */
   ('2025-12-15','TJ Walnuts',1,1,'2025-12-18',2),
   ('2025-12-15','Kite Hill Greek Style Plant-Based Yogurt',1,2,'2025-12-17',1),
