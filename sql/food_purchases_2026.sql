b664fa57ecf4b0dfde500bff398ec488a5c90f2e
Vipul Naik <vipulnaik1@gmail.com>
2026-01-09 23:14:59 -0800

    Add Friday 2026-01-09 Trader Joe's trip

--- sql/food_purchases_2026.sql
+++ sql/food_purchases_2026.sql
@@ -12,7 +12,15 @@ insert into food_purchases(purchase_date, food_type, quantity) values
   ('2026-01-07','Whole Foods Walnuts',4),
   /* 2026-01-07 (Wednesday) Berkeley Bowl; total cost $70.40; checkout at 6:38 PM; paid by credit card; 12 items in transaction of which 4 were Seventh Generation dish soap bottles so 8 food items */
   ('2026-01-07','Lundberg Sustainable California White Basmati Rice',2),
-  ('2026-01-07','Kite Hill Greek Style Plant-Based Yogurt',6);
+  ('2026-01-07','Kite Hill Greek Style Plant-Based Yogurt',6),
+  /* 2026-01-09 (Friday) Trader Joe's; total cost $27.07; checkout at 6:44 PM; paid by debit card; 13 items in transaction */
+  ('2026-01-09','TJ Miso Ginger Broth',2),
+  ('2026-01-09','TJ English Shelled Peas',2),
+  ('2026-01-09','TJ Roasted Seaweed',1),
+  ('2026-01-09','TJ Almond Milk',1), /* bulges out a bit more than usual, hard to fit in backpack and fridge! */
+  ('2026-01-09','TJ Kale',1),
+  ('2026-01-09','Red bell pepper',2),
+  ('2026-01-09','Green bell pepper',4);
   /* after entering a batch of data for a purchase:
   (a) eyeball for correct format
   (b) double-check date and C-s search to confirm correctness
