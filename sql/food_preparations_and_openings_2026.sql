1e4ed2ac7a0afe6f96866053cc0f0e2f29409e2b
Vipul Naik <vipulnaik1@gmail.com>
2026-01-15 23:55:02 -0800

    Record an opening

--- sql/food_preparations_and_openings_2026.sql
+++ sql/food_preparations_and_openings_2026.sql
@@ -70,5 +70,6 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2026-01-14','Beefsteak tomato',6,2,null,null),
   /* new bottle/packet openings as the old ones finished */
   ('2026-01-15','Kite Hill Greek Style Plant-Based Yogurt',1,1,null,null),
-  ('2026-01-15','Udupi Palace spinach masala dosa free red chutney',1,1,null,null);
+  ('2026-01-15','Udupi Palace spinach masala dosa free red chutney',1,1,null,null),
+  ('2026-01-15','TJ Carb Savvy Tortillas',1,2,null,null);
   /* Double-check the date and meal index before committing */
