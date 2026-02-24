856d7f0f5df8e9df27e72412abb4b7c8742932a8
Vipul Naik <vipulnaik1@gmail.com>
2026-02-23 17:14:00 -0800

    Make several improvements to interpolated nutrient consumption verification queries
    * Switch nutrient_consumption_in_recent_week from temporary table to
      regular table for ease of followup digging after a verification
      query flags stuff
    
    * Add another table food_and_nutrient_consumption_in_recent_week
      with details at the food type level
    
    * Switch from avg to sum / 7 which is more robust to missed days (this
      was less of an issue with nutrient_consumption_in_recent_week which
      is unlikely to be missing for a whole day, but is more of an issue
      with the newly added food_and_nutrient_consumption_in_recent_week as
      specific foods may be missing for a particular day)
    
    * Include 'dig deeper' queries in comments at the end of a few
      verification queries to facilitate quickly digging deeper when a
      verification query flags stuff

--- python/interpolated_nutrient_consumption_verification_queries.py
+++ python/interpolated_nutrient_consumption_verification_queries.py
@@ -2,20 +2,39 @@ import shared, connection
 
 queries = [
     """
-    drop temporary table if exists nutrient_consumption_in_recent_week;
+    drop table if exists food_and_nutrient_consumption_in_recent_week;
 
-    create temporary table nutrient_consumption_in_recent_week as
+    create table food_and_nutrient_consumption_in_recent_week as
+    select food_type, sum(interpolated_quantity) / 7 as daily_quantity_avg,
+    sum(calories) / 7 as daily_calories_avg,
+    sum(total_carb_in_grams) / 7 as daily_total_carb_in_grams_avg,
+    sum(total_fat_in_grams) / 7 as daily_total_fat_in_grams_avg,
+    sum(saturated_fat_in_grams) / 7 as daily_saturated_fat_in_grams_avg,
+    sum(protein_in_grams) / 7 as daily_protein_in_grams_avg,
+    sum(oxalate_in_mg) / 7 as daily_oxalate_in_mg_avg,
+    sum(calcium_in_mg) / 7 as daily_calcium_in_mg_avg,
+    sum(sodium_in_mg) / 7 as daily_sodium_in_mg_avg,
+    sum(potassium_in_mg) / 7 as daily_potassium_in_mg_avg,
+    sum(vitamin_a_in_mcg) / 7 as daily_vitamin_a_in_mcg_avg
+    from interpolated_food_and_nutrient_consumption
+    where datediff(curdate(), consumption_date) between 3 and 9 -- 1 week ending 3 days ago so there should be mostly complete / accurate data until then
+    group by food_type;""",
+
+    """
+    drop table if exists nutrient_consumption_in_recent_week;
+
+    create table nutrient_consumption_in_recent_week as
     select min(calories) as daily_calories_min, max(calories) as daily_calories_max,
-    avg(calories) as daily_calories_avg,
-    avg(total_carb_in_grams) as daily_total_carb_in_grams_avg,
-    avg(total_fat_in_grams) as daily_total_fat_in_grams_avg,
-    avg(saturated_fat_in_grams) as daily_saturated_fat_in_grams_avg,
-    avg(protein_in_grams) as daily_protein_in_grams_avg,
-    avg(oxalate_in_mg) as daily_oxalate_in_mg_avg,
-    avg(calcium_in_mg) as daily_calcium_in_mg_avg,
-    avg(sodium_in_mg) as daily_sodium_in_mg_avg,
-    avg(potassium_in_mg) as daily_potassium_in_mg_avg,
-    avg(vitamin_a_in_mcg) as daily_vitamin_a_in_mcg_avg
+    sum(calories) / 7 as daily_calories_avg,
+    sum(total_carb_in_grams) / 7 as daily_total_carb_in_grams_avg,
+    sum(total_fat_in_grams) / 7 as daily_total_fat_in_grams_avg,
+    sum(saturated_fat_in_grams) / 7 as daily_saturated_fat_in_grams_avg,
+    sum(protein_in_grams) / 7 as daily_protein_in_grams_avg,
+    sum(oxalate_in_mg) / 7 as daily_oxalate_in_mg_avg,
+    sum(calcium_in_mg) / 7 as daily_calcium_in_mg_avg,
+    sum(sodium_in_mg) / 7 as daily_sodium_in_mg_avg,
+    sum(potassium_in_mg) / 7 as daily_potassium_in_mg_avg,
+    sum(vitamin_a_in_mcg) / 7 as daily_vitamin_a_in_mcg_avg
     from interpolated_nutrient_consumption
     where datediff(curdate(), consumption_date) between 3 and 9; -- 1 week ending 3 days ago so there should be mostly complete / accurate data until then""",
 
@@ -47,15 +66,15 @@ queries = [
     # protein I bound from below and above in grams based on an upper estimate of my body weight
     "select daily_protein_in_grams_avg from nutrient_consumption_in_recent_week where daily_protein_in_grams_avg < 0.9 * 58 or daily_protein_in_grams_avg > 2.1 * 58;",
 
-    "select daily_oxalate_in_mg_avg from nutrient_consumption_in_recent_week where daily_oxalate_in_mg_avg > 150;",
+    "select daily_oxalate_in_mg_avg from nutrient_consumption_in_recent_week where daily_oxalate_in_mg_avg > 150; -- dig deeper: select food_type, daily_quantity_avg, daily_oxalate_in_mg_avg from food_and_nutrient_consumption_in_recent_week order by daily_oxalate_in_mg_avg desc;",
 
-    "select daily_calcium_in_mg_avg from nutrient_consumption_in_recent_week where daily_calcium_in_mg_avg < 650 or daily_calcium_in_mg_avg > 2500;",
+    "select daily_calcium_in_mg_avg from nutrient_consumption_in_recent_week where daily_calcium_in_mg_avg < 650 or daily_calcium_in_mg_avg > 2500; -- dig deeper: select food_type, daily_quantity_avg, daily_calcium_in_mg_avg from food_and_nutrient_consumption_in_recent_week order by daily_calcium_in_mg_avg desc;",
 
-    "select daily_sodium_in_mg_avg from nutrient_consumption_in_recent_week where daily_sodium_in_mg_avg > 4500;",
+    "select daily_sodium_in_mg_avg from nutrient_consumption_in_recent_week where daily_sodium_in_mg_avg > 4500; -- dig deeper: select food_type, daily_quantity_avg, daily_sodium_in_mg_avg from food_and_nutrient_consumption_in_recent_week order by daily_sodium_in_mg_avg desc;",
 
-    "select daily_potassium_in_mg_avg from nutrient_consumption_in_recent_week where daily_potassium_in_mg_avg < 3000 or daily_potassium_in_mg_avg > 5400;",
+    "select daily_potassium_in_mg_avg from nutrient_consumption_in_recent_week where daily_potassium_in_mg_avg < 3000 or daily_potassium_in_mg_avg > 5400; -- dig deeper: select food_type, daily_quantity_avg, daily_potassium_in_mg_avg from food_and_nutrient_consumption_in_recent_week order by daily_potassium_in_mg_avg desc;",
 
-    "select daily_vitamin_a_in_mcg_avg from nutrient_consumption_in_recent_week where daily_vitamin_a_in_mcg_avg < 900 or daily_vitamin_a_in_mcg_avg > 4100;",
+    "select daily_vitamin_a_in_mcg_avg from nutrient_consumption_in_recent_week where daily_vitamin_a_in_mcg_avg < 900 or daily_vitamin_a_in_mcg_avg > 4100; -- dig deeper: select food_type, daily_quantity_avg, daily_vitamin_a_in_mcg_avg from food_and_nutrient_consumption_in_recent_week order by daily_vitamin_a_in_mcg_avg desc;",
 ]
 
 _connection = connection.connect()
