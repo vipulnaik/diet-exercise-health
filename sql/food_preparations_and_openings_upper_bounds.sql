4a3f91cd292dbf55bac9902ecdea2424f6e102e0
Vipul Naik <vipulnaik1@gmail.com>
2026-02-27 17:43:23 -0800

    Increase upper bound for vegan yogurt consumption

--- sql/food_preparations_and_openings_upper_bounds.sql
+++ sql/food_preparations_and_openings_upper_bounds.sql
@@ -15,5 +15,5 @@ insert into food_preparations_and_openings_upper_bounds(food_type, num_days, qua
   ('TJ Miso Ginger Broth',4,1),
   ('TJ Sauerkraut',6,1),
   ('Whole Foods Walnuts',4,1),
-  ('Kite Hill Greek Style Plant-Based Yogurt',10,6),
+  ('Kite Hill Greek Style Plant-Based Yogurt',10,7),
   ('Udupi Palace spinach masala dosa',7,2);
