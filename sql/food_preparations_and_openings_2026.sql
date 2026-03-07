73fc39b310f58e405f3fdbbacca74a97c6abd8ca
Vipul Naik <vipulnaik1@gmail.com>
2026-03-06 23:33:41 -0800

    Record a completion

--- sql/food_preparations_and_openings_2026.sql
+++ sql/food_preparations_and_openings_2026.sql
@@ -305,6 +305,6 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2026-03-05','Beefsteak tomato',3,2,null,null),
   /* new bottle/packet openings as the old ones finished */
   ('2026-03-05','TJ Roasted Seaweed',1,2,null,null),
-  ('2026-03-05','TJ Carb Savvy Tortillas',1,2,null,null),
+  ('2026-03-05','TJ Carb Savvy Tortillas',1,2,'2026-03-06',2),
   ('2026-03-06','Kite Hill Greek Style Plant-Based Yogurt',1,1,null,null);
   /* Double-check the date and meal index before committing */
