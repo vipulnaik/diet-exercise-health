8e93f91da09ba13f60966e2639e80721f48a90b2
Vipul Naik <vipulnaik1@gmail.com>
2026-03-17 17:24:44 -0700

    Add column to food_purchases SQL table for number of meals consumed so far; remove backquotes in schema; fill values for newly added column for food purchases in March 2026

--- sql/food_purchases_schema.sql
+++ sql/food_purchases_schema.sql
@@ -1,5 +1,6 @@
 create table food_purchases(
-  `purchase_date` date not null,
-  `food_type` varchar(200),
-  `quantity` decimal(8,4)
+  purchase_date date not null,
+  food_type varchar(200),
+  quantity decimal(8,4),
+  number_of_meals_consumed_so_far integer
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
