2049e75557304eaa410234eb8b0504d15ec9ecf9
Vipul Naik <vipulnaik1@gmail.com>
2026-03-20 00:17:51 -0700

    Retrospectively record olive oil bottle completion

--- sql/food_preparations_and_openings_2026.sql
+++ sql/food_preparations_and_openings_2026.sql
@@ -139,7 +139,7 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2026-01-29','TJ English Shelled Peas',2,2,'2026-02-04',1),
   ('2026-01-29','Beefsteak tomato',5,2,'2026-02-04',1),
   /* new bottle/packet openings as the old ones finished */
-  ('2026-01-29','Trader Giotto''s Olive Oil',1,2,null,null), /* opened with potatoes prep */
+  ('2026-01-29','Trader Giotto''s Olive Oil',1,2,'2026-03-18',2), /* opened with potatoes prep */
   ('2026-01-29','TJ Carb Savvy Tortillas',1,2,'2026-01-30',2),
   ('2026-01-30','Kite Hill Greek Style Plant-Based Yogurt',1,1,'2026-01-31',1),
   ('2026-01-30','TJ Carb Savvy Tortillas',1,2,'2026-01-31',2),
