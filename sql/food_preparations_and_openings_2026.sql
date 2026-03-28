7f18c2fdd4412821e5da230d25ff4d6001a96e33
Vipul Naik <vipulnaik1@gmail.com>
2026-03-27 23:13:00 -0700

    Record a completion

--- sql/food_preparations_and_openings_2026.sql
+++ sql/food_preparations_and_openings_2026.sql
@@ -361,7 +361,7 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2026-03-18','Beefsteak tomato',9,2,null,null), /* checkpoint: stock down to 0 */
   /* new bottle/packet openings as the old ones finished */
   ('2026-03-18','Morton Iodized Sea Salt',1,2,null,null), /* opened with eggplant meal prep */
-  ('2026-03-18','TJ Roasted Seaweed',1,2,null,null),
+  ('2026-03-18','TJ Roasted Seaweed',1,2,'2026-03-27',2),
   ('2026-03-18','Kite Hill Greek Style Plant-Based Yogurt',1,2,'2026-03-20',2),
   /* 2026-03-20 rice prep (second meal) (added turmeric, cumin, salt) */
   ('2026-03-20','Lundberg Sustainable California White Jasmine Rice',1,2,'2026-03-24',1),
