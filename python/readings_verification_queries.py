4b7eb8b09de398a9b6b00b72aa0c8c9b69823dbc
Vipul Naik <vipulnaik1@gmail.com>
2026-04-13 15:02:17 -0700

    Add readings verification queries to verify that there are no readings/measurements with date in the future

--- python/readings_verification_queries.py
+++ python/readings_verification_queries.py
@@ -2,6 +2,8 @@ import shared, connection
 
 queries = [
     # Blood pressure
+    "select * from blood_pressure_readings where reading_date > curdate();",
+
     "select * from blood_pressure_readings where measurer = 'self' and not unusual_circumstances and not (systolic_bp_mm_hg between 90 and 120);",
 
     "select * from blood_pressure_readings where measurer = 'self' and not unusual_circumstances and not (diastolic_bp_mm_hg between 55 and 85);",
@@ -16,6 +18,8 @@ queries = [
     "select * from breath_counts where activity_mode = 'jogging' and not ((60 * breath_count) / time_period_in_seconds) between 13 and 17;",
 
     # Pulse oximeter
+    "select * from pulse_oximeter_readings where reading_date > curdate();",
+
     "select * from pulse_oximeter_readings where not (within_an_hour_after_exercise or within_two_hours_after_eating) and not (heart_rate_min_bpm between 40 and 65) and not (reading_date = '2024-08-27');",
 
     "select * from pulse_oximeter_readings where not (within_an_hour_after_exercise or within_two_hours_after_eating) and not (heart_rate_max_bpm between 45 and 70) and not (reading_date in ('2024-08-27','2024-12-14'));",
@@ -29,6 +33,8 @@ queries = [
     "select * from pulse_oximeter_readings where not (spo2_max between 96 and 100) and not ((reading_date, index_within_reading_date) in (('2025-06-10',1)));",
 
     # Stethoscope heart rate measurements
+    "select * from stethoscope_heart_rate_measurements where measurement_date > curdate();",
+
     "select * from stethoscope_heart_rate_measurements where not unusual_circumstances and not (heart_rate_bpm between 40 and 70);",
 ]
 
