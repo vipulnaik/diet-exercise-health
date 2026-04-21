6a64c7388f371436a316b5c2bf48d99d94f3a345
Vipul Naik <vipulnaik1@gmail.com>
2026-04-20 23:38:39 -0700

    Record potatoes prep

--- sql/food_preparations_and_openings_2026.sql
+++ sql/food_preparations_and_openings_2026.sql
@@ -486,5 +486,10 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2026-04-19','Ezekiel Sprouted Flourless Tortillas',1,2,null,null),
   ('2026-04-20','Kite Hill Greek Style Plant-Based Yogurt',1,1,null,null),
   ('2026-04-20','TJ Miso Ginger Broth',1,1,null,null),
-  ('2026-04-20','TJ Sauerkraut',1,1,null,null);
+  ('2026-04-20','TJ Sauerkraut',1,1,null,null),
+  /* 2026-04-20 potatoes prep (second meal) (added olive oil, turmeric, cumin, cinnamon, cayenne pepper (a little less than usual), and salt) */
+  ('2026-04-20','Gold potatoes 5 lb',0.5,2,null,null),
+  ('2026-04-20','TJ Kale',1,2,null,null),
+  ('2026-04-20','TJ Carrots',1,2,null,null),
+  ('2026-04-20','Beefsteak tomato',3,2,null,null); /* limited by space in pot, not by tomato stock */
   /* Double-check the date and meal index before committing */
