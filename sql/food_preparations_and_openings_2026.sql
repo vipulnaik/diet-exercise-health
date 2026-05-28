fbc074e056cb875228cf3b0d1c6e091e7a434629
Vipul Naik <vipulnaik1@gmail.com>
2026-05-27 22:03:25 -0700

    Record a completion

--- sql/food_preparations_and_openings_2026.sql
+++ sql/food_preparations_and_openings_2026.sql
@@ -635,7 +635,7 @@ insert into food_preparations_and_openings(preparation_or_opening_date, food_typ
   ('2026-05-24','Kite Hill Greek Style Plant-Based Yogurt',1,1,'2026-05-25',2),
   ('2026-05-25','TJ Miso Ginger Broth',1,1,null,null),
   ('2026-05-25','TJ Roasted Seaweed',1,2,null,null),
-  ('2026-05-25','Kite Hill Greek Style Plant-Based Yogurt',1,2,null,null),
+  ('2026-05-25','Kite Hill Greek Style Plant-Based Yogurt',1,2,'2026-05-27',2),
   /* 2026-05-26 rice prep (first meal) (added turmeric, cumin, salt) */
   ('2026-05-26','Lundberg Sustainable California White Basmati Rice',1,1,null,null),
   /* new bottle/packet openings as the old ones finished */
